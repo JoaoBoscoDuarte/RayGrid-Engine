7f7a58d15d9fb6efed5341fc5c38b41480c92c70
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2026-03-13 15:12:35 -0300

    .

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char** args) {
+	printf("Testando abertura de janela\n");
+	
+	// Pointers to our window and surface
+	SDL_Surface* winSurface = NULL;
+	
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	return 0;
+}
+
